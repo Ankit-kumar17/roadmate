912590b252878ed12a55ce36ff06473c87bab8ba
ankit kumar <ankits.kumar.cg@gmail.com>
2026-08-30 17:40:14 +0530

    feat: add trip card component to home and updated ui in home

--- mobile/app/(tabs)/home.js
+++ mobile/app/(tabs)/home.js
@@ -45,24 +45,24 @@ export default function Home() {
   );
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
 
-//     backgroundColor: "#F4F7FB",
+    backgroundColor: "#F4F7FB",
 
-//     paddingHorizontal: 20,
+    paddingHorizontal: 20,
 
-//     paddingTop: 25,
-//   },
+    paddingTop: 25,
+  },
 
-//   heading: {
-//     fontSize: 22,
+  heading: {
+    fontSize: 22,
 
-//     fontWeight: "700",
+    fontWeight: "700",
 
-//     color: "#111827",
+    color: "#111827",
 
-//     marginBottom: 15,
-//   },
-// });
+    marginBottom: 15,
+  },
+});
