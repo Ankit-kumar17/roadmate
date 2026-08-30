119e2103173f11dc2d403ecef5591b781c048ed1
ankit kumar <ankits.kumar.cg@gmail.com>
2026-08-30 15:21:55 +0530

    feat: add home screen

---
+++ mobile/app/(tabs)/home.js
@@ -0,0 +1,42 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+} from "react-native";
+
+import Header from "../../components/Header";
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+
+      <Header
+        name="Ankit"
+        subtitle="Ready for your next adventure?"
+      />
+
+      <Text style={styles.heading}>
+        Your Journey
+      </Text>
+
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+
+//     backgroundColor: "#F4F7FB",
+
+//     paddingHorizontal: 20,
+//     paddingTop: 25,
+//   },
+
+//   heading: {
+//     fontSize: 22,
+//     fontWeight: "700",
+
+//     color: "#111827",
+//   },
+// });
