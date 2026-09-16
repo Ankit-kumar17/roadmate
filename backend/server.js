9fe05f1e026ab24dcd09a5aa5fee0c0ed9001a38
ankit kumar <ankits.kumar.cg@gmail.com>
2026-09-16 23:45:15 +0530

    created backed

---
+++ backend/server.js
@@ -0,0 +1,54 @@
+// const express = require("express");
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// const app = express();
+
+// const port = process.env.port || 5000;
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("MongoDB connected");
+//   })
+//   .catch((error) => {
+//     console.log("MongoDB connection error:", error);
+//   });
+
+
+// app.get("/", (req,res) => {
+// res.send("backend is running")
+// })
+
+// app.listen(port, () => {
+//     console.log(`server is running on port ${port}`)
+// } )
+
+
+const dns = require("dns");
+
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+  })
+  .catch((error) => {
+    console.log("MongoDB connection error:", error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("backend is running");
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
