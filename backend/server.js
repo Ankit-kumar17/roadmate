6a55c639361a270d0885556965e0bbc1a587471b
ankit kumar <ankits.kumar.cg@gmail.com>
2026-09-17 08:38:20 +0530

    updated server.js that frontend dends data in json form

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,3 @@
-// const express = require("express");
-// const mongoose = require("mongoose");
-// require("dotenv").config();
-// const app = express();
-
-// const port = process.env.port || 5000;
-
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     console.log("MongoDB connected");
-//   })
-//   .catch((error) => {
-//     console.log("MongoDB connection error:", error);
-//   });
-
-
-// app.get("/", (req,res) => {
-// res.send("backend is running")
-// })
-
-// app.listen(port, () => {
-//     console.log(`server is running on port ${port}`)
-// } )
 
 
 const dns = require("dns");
@@ -33,9 +9,9 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-
+app.use(express.json);
 const port = process.env.PORT || 5000;
-
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
