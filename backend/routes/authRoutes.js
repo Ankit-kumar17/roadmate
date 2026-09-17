ce56d8ff975ee59d8bbea942ba4cc88fb37cd26f
ankit kumar <ankits.kumar.cg@gmail.com>
2026-09-17 10:53:18 +0530

    added login and register route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,80 @@
+const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-const User = require("./models/User");
+const User = require("../models/User");
 
 const router = express.Router();
+
+router.post("/register",async (req,res) => {
+    try{
+        const {name, email, password} = req.body;
+        const existingUser = await User.findOne({email});
+
+        if(existingUser){
+            return res.status(400).json({
+                message:"User Already exists",
+            });
+        }
+
+        const hashedPasssword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,email,password:hashedPasssword,
+        });
+
+        res.status(201).json({
+            message: "User registered successfully",
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email
+            },
+        });
+    }
+    catch(error) {
+        res.status(500).json({
+            message:"Server error",
+        })
+    }
+});
+
+router.post("/login", async(req,res) =>{
+    try{
+        const{email,password} = req.body;
+
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({
+                message:"invalid email or password ",
+            });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if(!isPasswordCorrect){
+            return res.status(400).json({
+                message:"Invalid email or passwword",
+            })
+        };
+
+        res.status(200).json({
+            message:"login successful",
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+            },
+        });
+    }
+    catch(error){
+        res.status(500).json({
+            message:"server error",
+        })
+        
+    }
+})
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,11 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const express = require("express");
 const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 require("dotenv").config();
 
 const app = express();
-app.use(express.json);
+app.use(express.json());
 const port = process.env.PORT || 5000;
 app.use(express.json());
 mongoose
@@ -21,6 +22,8 @@ mongoose
     console.log("MongoDB connection error:", error);
   });
 
+
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("backend is running");
 });
